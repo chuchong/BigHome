dce364b7ae1ae564dfccd82a389d5a7c4445bc1c
chuchong <33688310+chuchong@users.noreply.github.com>
2019-07-18 20:06:11 +0800

    试着将shooter逻辑分离到director中并做了无敌帧

--- assets/Script/BattleScene.js
+++ assets/Script/BattleScene.js
@@ -8,36 +8,77 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
+// 无敌时间
+function throttle (fn, time = 500) {
+  let timer
+  return function (...args) {
+    if (timer == null) {
+      fn.apply(this, args)
+      timer = setTimeout(() => {
+        timer = null
+      }, time)
+    }
+  }
+}
+
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-    },
+  properties: {
+    shooter: cc.Node,
+    scoreLabel: cc.RichText,
+    life: {
+      default: 4,
+      type: cc.Integer
+    }
+    // foo: {
+    //     // ATTRIBUTES:
+    //     default: null,        // The default value will be used only when the component attaching
+    //                           // to a node for the first time
+    //     type: cc.SpriteFrame, // optional, default is typeof default
+    //     serializable: true,   // optional, default is true
+    // },
+    // bar: {
+    //     get () {
+    //         return this._bar;
+    //     },
+    //     set (value) {
+    //         this._bar = value;
+    //     }
+    // },
+  },
 
-    // LIFE-CYCLE CALLBACKS:
+  // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
-        cc.director.getPhysicsManager().enabled = true;
-    },
+  onLoad () {
+    // cc.director.getPhysicsManager().enabled = true
+    this.shooter.on('shooter_attacked', throttle(function (event) {
+      console.log(this)
+      this.life--
+      this.update()
+      event.stopPropagation()
+    }), this)
+  },
 
-    start () {
+  start () {
+    this.life = 5
+    this.scoreLabel.string = this.life.toString()
+    this.node.dispatchEvent(new cc.Event.EventCustom('shooter_attacked', true))
+  },
 
-    },
+  copeShooterContact: function () {
+    this.life--
+    this.update()
+  },
 
-    // update (dt) {},
-});
+  changeToNextScene: function () {
+    cc.director.loadScene('StartScene')
+  },
+
+  update (dt) {
+    if (this.life <= 0) {
+      this.changeToNextScene()
+    }
+    this.scoreLabel.string = '剩余生命' + this.life.toString()
+  }
+})
--- assets/Script/shooter.js
+++ assets/Script/shooter.js
@@ -9,87 +9,83 @@
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        speed: 1000,
-        bullet_speed: 300,
-        bullet: {
-            default: null,
-            type: cc.Node
-        },
-        x_up_limit: 400,
-        x_down_limit: -400,
-        y_up_limit: 570,
-        y_down_limit: 70,
-        renew_frequency: 10
+  properties: {
+    director: cc.Node,
+    speed: 1000,
+    bullet_speed: 300,
+    bullet: {
+      default: null,
+      type: cc.Node
     },
+    x_up_limit: 400,
+    x_down_limit: -400,
+    y_up_limit: 570,
+    y_down_limit: 70,
+    renew_frequency: 10
+  },
 
-    // LIFE-CYCLE CALLBACKS:
+  // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
-        let manager = cc.director.getPhysicsManager()
-        manager.enabled = true
-        this.life = 5
-        this.score = 0
-        this.count = 0
-    },
+  onLoad () {
+    let manager = cc.director.getPhysicsManager()
+    manager.enabled = true
+    // this.life = 5
+    // this.score = 0
+    // this.count = 0
+  },
 
-    rad2deg: function (x){
-        return x * 180 / Math.PI;
-    },
+  rad2deg: function (x) {
+    return x * 180 / Math.PI
+  },
 
-    onTouchBegan: function (event) {
-        let touchLoc = event.touch.getLocation();        
-        let self = this.node.convertToWorldSpaceAR(new cc.Vec2(0,0))
-        let angle = Math.atan2(touchLoc.y - self.y, touchLoc.x - self.x)
-        this.node.rotation = this.rad2deg(Math.PI / 2 - angle)
-        this.rigidbody.linearVelocity = cc.v2(-this.speed * Math.cos(angle), -this.speed * Math.sin(angle));
-        this.rigidbody.angularVelocity = 0
+  onTouchBegan: function (event) {
+    let touchLoc = event.touch.getLocation()
+    let self = this.node.convertToWorldSpaceAR(new cc.Vec2(0, 0))
+    let angle = Math.atan2(touchLoc.y - self.y, touchLoc.x - self.x)
+    this.node.rotation = this.rad2deg(Math.PI / 2 - angle)
+    this.rigidbody.linearVelocity = cc.v2(-this.speed * Math.cos(angle), -this.speed * Math.sin(angle))
+    this.rigidbody.angularVelocity = 0
 
-        let bullet = cc.instantiate(this.bullet);
-        bullet.position = this.node.convertToWorldSpaceAR(new cc.Vec2(0,60));
-        bullet.rotation = this.rad2deg(- angle) -45
-        bullet.active = true;
-        let rigid = bullet.getComponent(cc.RigidBody)
-        rigid.linearVelocity = cc.v2(this.bullet_speed * Math.cos(angle), this.bullet_speed * Math.sin(angle));
-        this.scene.addChild(bullet);
-        this.count++
-        this.cycle_counter = 0
-    },
+    let bullet = cc.instantiate(this.bullet)
+    bullet.position = this.node.convertToWorldSpaceAR(new cc.Vec2(0, 60))
+    bullet.rotation = this.rad2deg(-angle) - 45
+    bullet.active = true
+    let rigid = bullet.getComponent(cc.RigidBody)
+    rigid.linearVelocity = cc.v2(this.bullet_speed * Math.cos(angle), this.bullet_speed * Math.sin(angle))
+    this.scene.addChild(bullet)
+    this.count++
+    this.cycle_counter = 0
+  },
 
-    start () {
-        let canvas = cc.find('Canvas');
-        canvas.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
-        this.scene = cc.director.getScene();
-        this.rigidbody = this.node.getComponent(cc.RigidBody);
-    },
+  start () {
+    let canvas = cc.find('Canvas')
+    canvas.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this)
+    this.scene = cc.director.getScene()
+    this.rigidbody = this.node.getComponent(cc.RigidBody)
+  },
 
-    onBeginContact: function (contact, selfCollider, otherCollider) {
-        this.life--
-        if (this.life === 0){
-            cc.director.loadScene('FinishScene')
-        }
-    },
+  onBeginContact: function (contact, selfCollider, otherCollider) {
+    this.node.dispatchEvent(new cc.Event.EventCustom('shooter_attacked', true))
+  },
 
-    update (dt) {
-        this.score += this.count
-        if (this.cycle_counter % this.renew_frequency !== 0){
-            return
-        }
-        if (this.node.x > this.x_up_limit){
-            this.node.x = this.x_down_limit
-        }
-        else if (this.node.x < this.x_down_limit){
-            this.node.x = this.x_up_limit
-        }
-        if (this.node.y > this.y_up_limit){
-            this.node.y = this.y_down_limit
-        }
-        else if (this.node.y < this.y_down_limit){
-            this.node.y = this.y_up_limit
-        }
-        this.score_label = cc.find("Canvas/scoreLabel").getComponent(cc.RichText)
-        this.score_label.string = "Life: " + this.life.toString() + " Score: " + this.score.toString()
+  update (dt) {
+    this.score += this.count
+    if (this.cycle_counter % this.renew_frequency !== 0) {
+      return
     }
-});
+    if (this.node.x > this.x_up_limit) {
+      this.node.x = this.x_down_limit
+    } else if (this.node.x < this.x_down_limit) {
+      this.node.x = this.x_up_limit
+    }
+    if (this.node.y > this.y_up_limit) {
+      this.node.y = this.y_down_limit
+    } else if (this.node.y < this.y_down_limit) {
+      this.node.y = this.y_up_limit
+    }
+    // this.score_label = cc.find('Canvas/scoreLabel').getComponent(cc.RichText)
+    // this.score_label.string = 'Life: ' + this.life.toString() + ' Score: ' + this.score.toString()
+  }
+})
