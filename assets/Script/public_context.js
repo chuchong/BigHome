63fa94727e5384ab19c84caf39fce835eb0bf7b1
tgxs002 <38932123+tgxs002@users.noreply.github.com>
2019-07-20 01:28:21 +0800

    加入微信公共域

---
+++ assets/Script/public_context.js
@@ -0,0 +1,96 @@
+/**
+ * wx api: https://developers.weixin.qq.com/minigame/dev/document/open-api/user-info/wx.createUserInfoButton.html
+ */
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        wxSubContextView: cc.Node,
+
+        avatar: cc.Sprite,
+
+        background: cc.Node,
+    },
+
+    start () {
+        console.log("start")
+        this.initAction();
+        this.initUserInfoButton();
+    },
+
+    initAction () {
+        console.log("initaction")
+        this._isShow = false;
+        let size = cc.winSize;
+        this._showAction = cc.moveTo(0.5, this.wxSubContextView.x, Math.floor(100));
+        this._hideAction = cc.moveTo(0.5, this.wxSubContextView.x, Math.floor(size.height));
+
+        this.wxSubContextView.runAction(this._hideAction);
+
+        this.background.on('touchstart', this.onClick, this);
+    },
+
+    initUserInfoButton () {
+        console.log("inituser")
+        if (typeof wx === 'undefined') {
+            console.log("wx undefined")
+            return;
+        }
+
+        let systemInfo = wx.getSystemInfoSync();
+        let button = wx.createUserInfoButton({
+            type: 'text',
+            text: '',
+            style: {
+                left: 30,
+                top: 30,
+                width: 100,
+                height: 100,
+                lineHeight: 40,
+                backgroundColor: '#00000000',
+                color: '#00000000',
+                textAlign: 'center',
+                fontSize: 10,
+                borderRadius: 50
+            }
+        });
+
+        button.onTap((res) => {
+            console.log("registered")
+            let userInfo = res.userInfo;
+            if (!userInfo) {
+                return;
+            }
+
+            cc.loader.load({url: userInfo.avatarUrl, type: 'png'}, (err, texture) => {
+                console.log("image_loaded")
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                this.avatar.spriteFrame = new cc.SpriteFrame(texture);
+            });
+
+            wx.getOpenDataContext().postMessage({
+                message: "User info get success."
+            });
+
+            this.onClick()
+
+            button.hide();
+            button.destroy();
+
+        });
+    },
+
+    onClick () {
+        this._isShow = !this._isShow;
+        console.log("clicked")
+        if (this._isShow) {
+            this.wxSubContextView.runAction(this._showAction);
+        }
+        else {
+            this.wxSubContextView.runAction(this._hideAction);
+        }
+    },
+});
